fcd0bd6f0631c50fa38b08816962e33478c4c037
Jotaefi <joao981535075@gmail.com>
2026-04-23 22:17:11 -0300

    substituindo if para switch case e tirando a situacao do aluno

--- src/Main.java
+++ src/Main.java
@@ -14,30 +14,23 @@ public class Main {
             opcao = sc.nextInt();
             sc.nextLine(); // Limpa buffer
 
-            if (opcao == 1){
-                System.out.print("Nome do aluno: ");
-                aluno = sc.nextLine();
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome do aluno: ");
+                    aluno = sc.nextLine();
 
-                System.out.print("Primeira nota: ");
-                double nota1 = sc.nextDouble();
+                    System.out.print("Primeira nota: ");
+                    double nota1 = sc.nextDouble();
 
-                System.out.print("Segunda nota: ");
-                double nota2 = sc.nextDouble();
+                    System.out.print("Segunda nota: ");
+                    double nota2 = sc.nextDouble();
 
-                System.out.print("Terceira nota: ");
-                double nota3 = sc.nextDouble();
+                    System.out.print("Terceira nota: ");
+                    double nota3 = sc.nextDouble();
 
-                double media = (nota1 + nota2 + nota3) / 3;
-                System.out.printf("Media: %.2f%n", media);
-
-                    if (media >= 7){
-                        System.out.println("APROVADO!!");
-                    } else if (media <= 5){
-                        System.out.println("REPROVADO!!");
-                    } else {
-                        System.out.println("RECUPERACAO!!");
-                    }
-                break;
+                    double media = (nota1 + nota2 + nota3) / 3;
+                    System.out.printf("Media: %.2f%n", media);
+                    break;
             }
         } while(opcao != 0);
     }
