fcb11e9b47d6c37f5f105665aee2bb0efc742bf3
Jotaefi <joao981535075@gmail.com>
2026-04-27 10:49:20 -0300

    menu novo e funcionalidade de array

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int opcao;
-        String aluno;
+        int contador = 0;
+        String[] alunos = new String[10];
 
         do {
-            System.out.println("[1] Media Aluno");
+            System.out.println("[1] Cadastrar aluno");
+            System.out.println("[2] Consultar aluno");
             System.out.println("[0] Sair");
             System.out.print("Opcao: ");
 
@@ -16,20 +18,13 @@ public class Main {
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Nome do aluno: ");
-                    aluno = sc.nextLine();
-
-                    System.out.print("Primeira nota: ");
-                    double nota1 = sc.nextDouble();
-
-                    System.out.print("Segunda nota: ");
-                    double nota2 = sc.nextDouble();
-
-                    System.out.print("Terceira nota: ");
-                    double nota3 = sc.nextDouble();
-
-                    double media = (nota1 + nota2 + nota3) / 3;
-                    System.out.printf("Media: %.2f%n", media);
+                        if (contador < alunos.length) {
+                            System.out.print("Aluno: ");
+                            alunos[contador] = sc.nextLine();
+                            contador++;
+                        } else {
+                            System.out.println("Cadastro cheio!!");
+                        }
                     break;
             }
         } while(opcao != 0);
