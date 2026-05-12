b3914f14bc91b5da28166db1d27ac83f1c7f6b60
Jotaefi <joao981535075@gmail.com>
2026-05-12 16:48:10 -0300

    adicionando service e importanto na classe principal

--- src/sistemaescolar/main/Main.java
+++ src/sistemaescolar/main/Main.java
@@ -1,24 +1,14 @@
 package sistemaescolar.main;
 
+import sistemaescolar.model.Alunos;
+import sistemaescolar.service.CadastroAluno;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int opcao;
-        ArrayList<String> alunos = new ArrayList<>();
 
-        do {
-            System.out.println("[1] Cadastrar aluno");
-            System.out.println("[2] Listar alunos");
-            System.out.println("[3] Remover aluno");
-            System.out.println("[0] Sair");
-            System.out.print("Opcao: ");
-
-            opcao = sc.nextInt();
-            sc.nextLine(); // Limpa buffer
-
-        } while(opcao != 0);
     }
 }
---
+++ src/sistemaescolar/service/CadastroAluno.java
@@ -0,0 +1,27 @@
+package sistemaescolar.service;
+
+import sistemaescolar.model.Alunos;
+import java.util.ArrayList;
+
+public class CadastroAluno {
+    ArrayList<Alunos> listaAlunos = new ArrayList<>();
+
+    //CADASTRAR
+    public void adicionarAluno (Alunos aluno){
+        listaAlunos.add(aluno);
+        System.out.println("Aluno cadastrado!");
+    }
+
+    //LISTAR
+    public void listarAlunos () {
+        if (listaAlunos.isEmpty()) {
+            System.out.println("Nenhum aluno cadastrado!");
+            return;
+        }
+        for (Alunos aluno: listaAlunos) {
+            System.out.println("Aluno: " + aluno.getNome());
+            System.out.println("Média: " + aluno.calcularMedia());
+            System.out.println("Situação: " + aluno.situacaoAluno());
+        }
+    }
+}
